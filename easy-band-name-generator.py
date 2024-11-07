10ec433e90a0d06f4e91860c12721ccee556604a
aliyahsagaban <aliyah.sagaban@aiqueinnovation.com>
2024-11-07 10:49:16 +0800

    initial commit

---
+++ easy-band-name-generator.py
@@ -0,0 +1,8 @@
+print("Welcome to Band Name Generator!")
+print("What's the name of the city you grew up with?")
+city = input()
+
+print("What's your pet's name?")
+pet = input()
+
+print("Your band name could be " + city + " " + pet)
